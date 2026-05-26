b1ac713fe7ec78bda51f52b477d64a15f434ebc9
GameMaster <281981779+GameMaster97@users.noreply.github.com>
2026-05-25 19:48:29 -0700

    Fix type effect and add more splashes

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,23 @@ console.log("Hello gamer!")
 localStorage.setItem("name", "Person");
 
 // function
-var typeEffect = (element, newText, spacingTimeMS, endingFunction) => {
+var typeEffect = (element, textToAdd, spacingTimeMS, endingFunction) => {
     // Variables to help us
-    newText = newText.split("");
-    const originalLen = newText.length;
+    textToAdd = textToAdd.split("");
+    const originalLen = textToAdd.length;
     let len = 0;
-    let currentText = "";
+    let currentText = element.innerHTML;
 
     // Do like the type effect
     const intervalId = setInterval(() => {
-        currentText = currentText + newText[len];
-        element.textContent = currentText + "|";
+        currentText = currentText + textToAdd[len];
+        element.innerHTML = currentText + "|";
 
         console.log(`Status: ${len} // ${originalLen}`);
     
         len++;
         if (len === originalLen) {
-            element.textContent = currentText; 
+            element.innerHTML = currentText; 
             clearInterval(intervalId);
             if (endingFunction !== undefined) {
                 endingFunction()
@@ -33,10 +33,20 @@ const fillerText = [
     "Don't waste all your money!",
     "Always keep an eye on your spending!",
     "Cancel useless subscriptions, please.",
+    `There is a ${Math.round(100 / fillerText.length).toString()}% chance that you will see this rare message!`,
     `My my, ${localStorage.getItem("name") || "Person"} is a wonderful name!`,
-    "How's your side hustle going?"
+    "How's your side hustle going?",
+    "Feels good to be productive, huh?",
+    "He sacs, THE ROOOOOOOOK!",
+    "Yeet the knight to f6, forking the queen and the rook.",
+    "They say e=mc<sup>2</sup>, so does that mean e=m(a<sup>2</sup>+b<sup>2</sup>?)",
+    "Game is.. something that's not game?",
+    "It is 7:43PM and I'm writing the stupidest splash texts.",
+    "Side hustles can sure be helpful.",
+    "A high-interest savings account works wonders.",
+    "Don't invest in TO-DA-MOON!",
 ];
-fillerText.push([`There is a ${Math.round(100 / (fillerText.length+1)).toString()}% chance that you will see this rare message!`]);
+
 typeEffect(welcomeText, `Welcome back, ${localStorage.getItem("name") || "Person"}.`, 100, () => {
     setTimeout(() => {
         typeEffect(welcomeText, fillerText[Math.floor(Math.random() * fillerText.length)], 75);
