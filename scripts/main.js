ade7a232db68a78e98094f494bebb8ef2d085979
GameMaster <281981779+GameMaster97@users.noreply.github.com>
2026-05-25 19:53:21 -0700

    Fix and splashes

--- scripts/main.js
+++ scripts/main.js
@@ -38,16 +38,20 @@ const fillerText = [
     "Feels good to be productive, huh?",
     "He sacs, THE ROOOOOOOOK!",
     "Yeet the knight to f6, forking the queen and the rook.",
-    "They say e=mc<sup>2</sup>, so does that mean e=m(a<sup>2</sup>+b<sup>2</sup>?)",
+    "They say e=mc<sup>2</sup>, so does that mean e=m(a<sup>2</sup>+b<sup>2</sup>)?",
     "Game is.. something that's not game?",
     "It is 7:43PM and I'm writing the stupidest splash texts.",
     "Side hustles can sure be helpful.",
     "A high-interest savings account works wonders.",
     "Don't invest in TO-DA-MOON!",
+    "The average return of the S&P 500 is 8-10% annually.",
+    "You wouldn't believe how much water AI Data Centers use.",
+    "Rammageddon - A huge price surge of ram (bad), thanks to AI!",
+    "So you're telling me I pay the same price for less monthly robux?",
 ];
 fillerText.push([`There is a ${Math.round(100 / (fillerText.length+1)).toString()}% chance that you will see this rare message!`])
 
-typeEffect(welcomeText, `Welcome back, ${localStorage.getItem("name") || "Person"}.`, 100, () => {
+typeEffect(welcomeText, `Welcome back, ${localStorage.getItem("name") || "Person"}. `, 100, () => {
     setTimeout(() => {
         typeEffect(welcomeText, fillerText[Math.floor(Math.random() * fillerText.length)], 75);
     }, 400);
